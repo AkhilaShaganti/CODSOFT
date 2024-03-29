cf63da5db50945e3e929f99af645d99a880328c8
AkhilaShaganti <146197903+AkhilaShaganti@users.noreply.github.com>
2024-03-29 20:37:01 +0530

    TASK_2(Student Grade Calculator).java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,3 +1,12 @@
+// TASK 2  STUDENT GRADE CALCULATOR
+
+//Input: Take marks obtained (out of 100) in each subject.
+// 1.Calculate Total Marks: Sum up the marks obtained in all subjects.
+// 2.Calculate Average Percentage: Divide the total marks by the total number of subjects to get the
+// average percentage.
+// 3.Grade Calculation: Assign grades based on the average percentage achieved.
+// 4.Display Results: Show the total marks, average percentage, and the corresponding grade to the user
+
 import java.util.Scanner;
 
 public class GradeCalculator {
