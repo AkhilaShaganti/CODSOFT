78000348ac5a7c9f97a297718d8898116c417a9a
AkhilaShaganti <146197903+AkhilaShaganti@users.noreply.github.com>
2024-03-29 20:41:05 +0530

    TASK_2 (Student Grade Calculator with GUI)

--- GradeCalculatorGUI.java
+++ GradeCalculatorGUI.java
@@ -1,3 +1,12 @@
+// TASK 2 STUDENT GRADE CALCULATOR WITH GUI
+
+//Input: Take marks obtained (out of 100) in each subject.
+//Calculate Total Marks: Sum up the marks obtained in all subjects.
+//Calculate Average Percentage: Divide the total marks by the total number of subjects to get the
+//average percentage.
+//Grade Calculation: Assign grades based on the average percentage achieved.
+//Display Results: Show the total marks, average percentage, and the corresponding grade to the user
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
