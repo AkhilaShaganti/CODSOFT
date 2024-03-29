be31dfd06aabfeaf4cca14c39ef368b583b6e983
AkhilaShaganti <146197903+AkhilaShaganti@users.noreply.github.com>
2024-03-29 21:27:47 +0530

    TASK_4 (Quiz Application With Timer).java

--- TASK_4 (Quiz Application With Timer).java
+++ TASK_4 (Quiz Application With Timer).java
@@ -1,3 +1,12 @@
+//Quiz Questions With Timer TASK 4 
+  // 1.  Store quiz questions along with multiple-choice options and correct answers.
+ // 2. Timer: Implement a timer for each question to limit the time to answer.
+ // 3. Question Display: Present one question at a time with multiple-choice options.
+ // 4.Answer Submission: Allow users to select an option and submit their answer within the given
+ //time.
+ // 5. Score Calculation: Keep track of the user's score based on correct answers.
+ // 6. Result Screen: Display the final score and a summary of correct/incorrect answers.
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
